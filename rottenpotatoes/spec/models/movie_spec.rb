45ed3f1cc2a1edd656176f0478d78c5580afe658
Stephen2526 <yuanfei25fairy@gmail.com>
2016-03-30 06:35:53 +0000

    frame of Rspec done

---
+++ rottenpotatoes/spec/models/movie_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Movie do
+    
+    it 'has a method called same_director' do
+        
+    end
+    
+    describe '#same_director' do
+       
+       it 'receives the director as a params' do
+           
+       end
+       
+       it 'returns the set of movies having same director' do
+           
+       end
+    end
+    
+end
